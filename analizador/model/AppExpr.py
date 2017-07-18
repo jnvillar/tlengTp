1bd97d7d4625303643debcec9bbed2f3f36f6629
jnvillar <juan.villar@mercadolibre.com>
2017-07-18 17:25:02 -0300

    app expresion

--- analizador/model/AppExpr.py
+++ analizador/model/AppExpr.py
@@ -1,24 +1,47 @@
+from Tipo import *
+
 
 class AppExpr(object):
     def __init__(self, expr1, expr2):
-        self.expr1 = expr1
-        self.expr2 = expr2
-       # self.expr = expr
-        self.tipo = None
-'''
+        self.exprDer = expr1
+        self.exprIzq = expr2
+        self.type = None
+        self.initialExpression = False
+        self.value = None
+
         # def __str__(self):
         #     return '\\'+str(self.var)+':'+ str(self.tipo) +'.'+str(self.expr)
 
 
-        def evaluar(self, context):
-            return toStr(self)
+        def evaluate(self, context, value=None):
+            self.exprDer.evaluate(context)
+            valDer = self.exprDer.getValue()
+            self.exprIzq.evaluate(context, valDer)
+            self.value = self.exprDer.getValue()
 
-        def tipar(self, context):
-            if self.expr1.tipar(context).dom != self.expr1.tipar(context).img
-                raise Exception(
-                    "La parte izquierda de la aplicacion no es una funcion con dominio en " + self.expr1.tipar(
-                        context).img)
+        def __str__(self):
+            toPrint = ""
+            if self.type == Tipo("Undefined"):
+                toPrint = str(self.exprDer) + str(self.exprIzq)
             else:
-                self.tipo = self.expr1.tipar(context).img
-                return self.tipo
-'''
+                toPrint = toPrint + str(expr1)
+
+            if self.initialExpression:
+                toPrint = toPrint + ":" + str(self.type)
+                toPrint = toPrint + str(self.exprDer.getType().img)
+
+            print toPrint
+
+        def setExprTypes(self, context):
+            self.exprDer.setExprTypes(context)
+            self.exprIzq.setExprTypes(context)
+
+            if self.exprDer.getType().dom != self.exprIzq.getType().img:
+                raise "No coinciden los tipos papa"
+            
+            if self.exprDer.getType() == Tipo('Nat'):
+                self.type = Tipo('Nat')
+            elif self.exprDer.getType() == Tipo('Bool'):
+                self.type = Tipo('Bool')
+            else:
+                self.type = Tipo('Undefined')
--- analizador/model/SuccExpr.py
+++ analizador/model/SuccExpr.py
@@ -11,7 +11,7 @@ class SuccExpr(object):
     def __str__(self):
         toPrint = ""
         if self.type == Tipo("Undefined"):
-            toPrint = "succ(" + str(expr) + ")" 
+            toPrint = "succ(" + str(expr) + ")"
         else:
             for it in range(self.value):
                 toPrint = toPrint + "succ("
@@ -19,31 +19,26 @@ class SuccExpr(object):
             for x in range(self.value):
                 toPrint = toPrint + ")"
 
-        if(self.initialExpression):
-            toPrint = toPrint+":"+str(self.type)
+        if (self.initialExpression):
+            toPrint = toPrint + ":" + str(self.type)
 
         return toPrint
 
-
     def evaluate(self, context):
-        
-        self.expr.evaluate(context)
-        if(self.expr.getType() == Tipo('Nat')):
-            self.value = self.expr.getValue() + 1
 
+        self.expr.evaluate(context)
+        if (self.expr.getType() == Tipo('Nat')):
+            self.value = self.expr.getValue() + 1
 
     def getType(self):
         return self.type
 
-
     def getValue(self):
         return self.value
 
-
     def setExprTypes(self, context):
         self.expr.setExprTypes(context)
-        if(self.expr.getType() == Tipo('Nat')):
+        if self.expr.getType() == Tipo('Nat'):
             self.type = Tipo('Nat')
         else:
             self.type = Tipo('Undefined')
-

05dd003960afd6331c0777e887605ea20e9fc8e7
jnvillar <juan.villar@mercadolibre.com>
2017-07-22 16:24:47 -0300

    is zero expr

--- analizador/model/IsZeroExpr.py
+++ analizador/model/IsZeroExpr.py
@@ -1,6 +1,5 @@
 from Tipo import *
 
-
 class IsZeroExpr(object):
     def __init__(self, expr):
         self.type = None
@@ -9,35 +8,40 @@ class IsZeroExpr(object):
         self.initialExpression = False
 
 
-def __str__(self):
-    toPrint = ""
-    if not self.defined:
-        toPrint = "IsZero("
-        toPrint = toPrint + str(self.expr)
-        toPrint = ")"
-    else:
-        print self.value == 0
+    def __str__(self):
+        toPrint = ""
+        if not self.defined:
+            toPrint = "IsZero("
+            toPrint = toPrint + str(self.expr)
+            toPrint = ")"
+        else:
+            print self.value == 0
 
-    if self.initialExpression:
-        toPrint = toPrint + ":" + str(self.type)
+        if self.initialExpression:
+            toPrint = toPrint + ":" + str(self.type)
 
-    print toPrint
-
-
-def evaluate(self, context):
-    self.expr.evaluate(context)
-    if self.expr.isDefined():
-        self.value = self.expr.getValue() == 0
-        self.defined = True
+        print toPrint
 
 
-def isDefined(self):
-    return self.defined
+    def evaluate(self, context):
+        self.expr.evaluate(context)
+        if self.expr.isDefined():
+            self.value = self.expr.getValue() == 0
+            self.defined = True
 
 
-def getType(self):
-    return self.type
+    def isDefined(self):
+        return self.defined
 
 
-def getValue(self):
-    return self.value == 0
+    def setExprTypes(self, context):
+        self.expr.setExprTypes(context)
+        if self.expr.getType() == Tipo('Nat'):git
+            self.type = Tipo('Bool')
+
+    def getType(self):
+        return self.type
+
+
+    def getValue(self):
+        return self.value == 0
